12b970841896ab4d86ddc71d999e55596e725b03
git_yi <ibonenko@yandex.ru>
2023-11-29 18:02:55 +0300

    Создан класс Bank

---
+++ Bank.py
@@ -0,0 +1,18 @@
+class Bank:
+	def __init__ (self):
+		self.clients = []
+
+	def add_client(self, client):
+		self.clients.append(client)
+
+	def del_clirnt(self, client):
+		for i in range(len(self.clients)):
+			if self.client[i].id == client.id;
+				self.client[i].pop()
+				break
+
+	def all_cash(self):
+		cash = 0
+		for i in range(len(self.clients)):
+			cash += self.clients[i].cash
+		return cash
